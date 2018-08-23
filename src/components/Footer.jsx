95120aaecd3c62197e9e6479efb2d05538f593f9
hitman <hitman@localhost.localdomain>
2018-08-23 15:32:25 +0700

    Center the footer and align the images with the text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,18 @@ import balloonImage from '../pages/images/hotairballoon-small.svg';
 import Link from 'gatsby-link';
 
 const Footer = (props) => (
-  <footer id="footer" style={{ display: `inline`}}>
-    <Link to="/" style={{float: 'left'}}>
-    <img style={{maxWidth: 57.5, maxHeight: 75 }} src={balloonImage} alt="Cool balloon" />
-    </Link>
-    <div className="copyright" style={{marginLeft: "1rem", paddingTop: "1rem"}}>
-      &copy; Bryan Willson Berry, This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a> <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
-    <img alt="Creative Commons License" style={{ borderWidth: 0, marginBottom: 0 }} src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a>
+  <footer id="footer" style={{ textAlign: "center", bottom: 0, display: "inline" }}>
+    <div className="copyright" style={{ marginLeft: "1rem", paddingTop: "1rem" }}>
+      <Link to="/">
+        <img style={{ maxWidth: 57.5, maxHeight: 75, verticalAlign: "middle" }} src={balloonImage} alt="Cool balloon" />
+      </Link>
+      &nbsp;&nbsp;&copy; Bryan Willson Berry,&nbsp;&nbsp;
+  <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
+        Creative Commons Attribution 4.0 International License
+  </a>
+      <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
+        &nbsp;&nbsp;<img alt="Creative Commons License" style={{ borderWidth: 0, marginBottom: 0, verticalAlign: "middle" }} src="https://i.creativecommons.org/l/by/4.0/88x31.png" />
+      </a>
     </div>
   </footer>
 );
