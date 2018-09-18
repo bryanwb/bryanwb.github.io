7641a3c616752eb4dd06b91b1e8fd6ea165c30e4
hitman <hitman@localhost.localdomain>
2018-09-18 16:11:11 +0700

    Implement pan with VX

--- learning/stock-charts/src/App.js
+++ learning/stock-charts/src/App.js
@@ -5,38 +5,40 @@ import './App.css';
 import BitcoinLogo from './images/Bitcoin_logo.svg';
 import DATA from './data/GOLD-BTC.json';
 import Chart from './components/Chart';
-import ChartButtons from './components/ChartButtons';
-import ZoomButtons from './components/ZoomButtons';
+import { Object } from 'core-js';
+/* import ChartButtons from './components/ChartButtons';
+ * import ZoomButtons from './components/ZoomButtons'; */
 
-const calculateDayCount = (range, max) => {
+// returns an offset from the end of the data
+const rangeToOffset = (range) => {
   const today = new Date(Date.now());
   const oneDay = (60.0 * 60.0 * 24.0);
-  let count;
+  let start;
   switch (range) {
     case 'YTD':
       const janFirst = new Date(`${today.getUTCFullYear()}-01-01`);
       // divide by 1000 to remove milliseconds
       const secsSinceJanFirst = (today.valueOf() - janFirst.valueOf()) / 1000;
-      count = Math.floor(secsSinceJanFirst / oneDay);
+      start = -Math.floor(secsSinceJanFirst / oneDay);
       break;
     case '7d':
-      count = 7;
+      start = -7;
       break;      
     case '1m':
-      count = 31;
+      start = -31;
       break;
     case '3m':
-      count = 93;
+      start = -93;
       break;
     case '1y':
-      count = 365;
+      start = -365;
       break;
     case 'ALL':
-      count = max;
+      start = -Infinity;
       break;
   }
 
-  return count;
+  return start;
 
 };
 
@@ -47,44 +49,40 @@ const ChartHeadline = (props) => {
     </div>
   );
 };
-/* const parseDataPoint = (d) => {
- *   let d1 = {}
- *   d1.date = new Date(+d.date);
- *   d1.open = +d.open;
- *   d1.high = +d.high;
- *   d1.low = +d.low;
- *   d1.close = +d.close;
- *   d1.volume = +d.volume;
- *   
- *   return d1;
- *     
- * };
- *  */
 
 const loadSeries = (showGold, showBtc) => {
+  let symbols = [];
   DATA.forEach(d => {
     d.date = new Date(d.date);
   });
-  DATA['symbols'] = [];
-  showGold && DATA['symbols'].push({name: 'GOLD', color: '#CFB53B', measure: '/kg'});
-  showBtc && DATA['symbols'].push({name: 'BTC', color: "#ff7f0e"});
-  return DATA;
+
+  showGold && symbols.push({name: 'GOLD', color: '#CFB53B', measure: '/kg'});
+  showBtc && symbols.push({name: 'BTC', color: "#ff7f0e"});
+  return [DATA, symbols];
+};
+
+const sliceByRange = (range, data) => {
+  const startOffset = range.start === -Infinity ? 0 : data.length + range.start;
+  const endOffset = data.length + range.end;
+  return data.slice(startOffset, endOffset)
 };
 
 class App extends Component {
   constructor(props) {
     super(props);
+    const start = rangeToOffset('YTD');
     this.state = {
       show: {
         marketCap: false,
         btc: true,
         gold: true,
       },
-      range: 'YTD',
+      range: {name: 'YTD', start: start, end: 0},
     };
     this.ranges = ['7d', '1m', '3m', '1y', 'YTD', 'ALL'];
     this.onChangeZoom = this.onChangeZoom.bind(this);
     this.onToggle = this.onToggle.bind(this);
+    this.onShift = this.onShift.bind(this);
   }
 
   onToggle(key, e) {
@@ -94,40 +92,66 @@ class App extends Component {
   }
 
   onChangeZoom(e) {
-    let copy = Object.assign({}, this.state);
-    copy.range = e.target.textContent;
-    this.setState(copy);
+    const rangeName = e.target.textContent;
+    const start = rangeToOffset(rangeName);
+    this.setState(Object.assign(this.state, {name: rangeName, start: start, end: 0}));
+  }
+
+  onShift(extentCount) {
+    const {range: name, start: start, end: end} = this.state.range;
+    let newStart = start + extentCount;
+    let newEnd = end + extentCount;
+    newStart = newStart > 0 ? -3 : newStart;
+    newEnd = newEnd > 0 ? 0: newEnd;
+    
+    const newRange = {name: name, start: newStart, end: newEnd};
+    this.setState(Object.assign(this.state, {range: newRange}));
   }
   
   render() {
     const width = 1200;
     const height = 700;
-    const data = loadSeries(this.state.show.gold, this.state.show.btc);
     const showMarketCap = this.state.show.marketCap;
     const margin = {left: 70, right: 70, top: 20, bottom: 30};
     const range = this.state.range;
+    const [DATA, symbols] = loadSeries(this.state.show.gold, this.state.show.btc);
+    const data = sliceByRange(range, DATA);
+    
     return (
       <div className="App" style={{margin: 'auto', marginTop: "2vh", width: width, height: height}}>
-          <ChartHeadline />
-          <div style={{writingMode: 'vertical-lr', height: "100%", textAlign: "center", float: "right"}}>Price (USD)</div>
           <Chart
             data={data}
+            symbols={symbols}
             width={width}
             height={height}
-            dayCount={calculateDayCount(range, data.length)}
+            shiftCb={this.onShift}
             showMarketCap={showMarketCap}
             margin={margin}
           />
-          <div style={{display: 'flex', marginLeft: margin.left}}>
-              <ZoomButtons
-                onClick={this.onChangeZoom}
-                ranges={this.ranges}
-                current={this.state.range}
-              />
-              <ChartButtons onToggle={this.onToggle} show={this.state.show} />
-          </div>
       </div>
-    );
+    )
+    /* return (
+     *   <div className="App" style={{margin: 'auto', marginTop: "2vh", width: width, height: height}}>
+     *       <ChartHeadline />
+     *       <div style={{writingMode: 'vertical-lr', height: "100%", textAlign: "center", float: "right"}}>Price (USD)</div>
+     *       <Chart
+     *         data={data}
+     *         width={width}
+     *         height={height}
+     *         dayCount={calculateDayCount(range, data.length)}
+     *         showMarketCap={showMarketCap}
+     *         margin={margin}
+     *       />
+     *       <div style={{display: 'flex', marginLeft: margin.left}}>
+     *           <ZoomButtons
+     *             onClick={this.onChangeZoom}
+     *             ranges={this.ranges}
+     *             current={this.state.range}
+     *           />
+     *           <ChartButtons onToggle={this.onToggle} show={this.state.show} />
+     *       </div>
+     *   </div>
+     * ); */
   }
 }
 
--- learning/stock-charts/src/components/Chart.js
+++ learning/stock-charts/src/components/Chart.js
@@ -1,178 +1,305 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { timeFormat } from "d3-time-format";
-import { format } from "d3-format";
-import {Chart, ChartCanvas} from 'react-stockcharts/es';
-import { XAxis, YAxis } from "react-stockcharts/es/lib/axes"
-import { LineSeries } from 'react-stockcharts/es/lib/series';
-import { discontinuousTimeScaleProvider } from "react-stockcharts/es/lib/scale";
-import { last } from "react-stockcharts/es/lib/utils";
-import {
-  CrossHairCursor,
-  MouseCoordinateX,
-  MouseCoordinateY,
-} from "react-stockcharts/es/lib/coordinates";
-import { HoverTooltip } from "react-stockcharts/es/lib/tooltip";
+import { Group } from '@vx/group';
+import { AxisRight, AxisBottom } from '@vx/axis';
+import { scaleTime, scaleLinear } from '@vx/scale';
+import { LinePath } from '@vx/shape';
+import { extent, max } from 'd3-array';
 
-
-const formatNum = (symbol, num) => {
-  let numFormatted = '$ ' + num.toFixed().toString().replace(/(\d)(?=(\d{3})+(?:\.\d+)?$)/g, "$1,");
-  if (symbol.measure) {
-    numFormatted += symbol.measure;
-  }
-  // this magic incantation adds commas in the thousands place
-  return numFormatted
-};
-
-// remove the hours:minutes:seconds
-const formatDate = (date) => {
-  return date.toUTCString().split(/\ \d\d:\d\d:\d\d/)[0];
-};
-
-const tooltipContent = (symbols) => {
-  return (d) => {
-    const {currentItem, xAccessor} = d;
-    const yValues = symbols.map(s => {
-      let items = [
-        {
-          label: s.name + ' ',
-          value: formatNum(s, currentItem[s.name].close),
-          stroke: '#000000'
-        },
-      ];
-      if (currentItem[s.name].cap) {
-        items.push(
-          {label: 'Market Cap',
-           value: '$ ' + format('.0s')(currentItem[s.name].cap),
-           stroke: '#000000'}
-        );
-      }
-      return items;
-    }).reduce((accum, arr) => accum.concat(arr), []);
-    return {
-      x: formatDate(xAccessor(currentItem)),
-      y: yValues,
-    };
-  };
-};
 
 class HotAirChart extends React.Component {
-  render() {
-    const showMarketCap = this.props.showMarketCap;
-    const xScaleProvider = discontinuousTimeScaleProvider
-      .inputDateAccessor(d => d.date);
-    const {
-      data,
-      xScale,
-      xAccessor,
-      displayXAccessor,
-    } = xScaleProvider(this.props.data);
-
-    const xExtents = [
-      xAccessor(data[data.length - this.props.dayCount]),
-      xAccessor(last(data)),
-    ];
-
-    const symbols = this.props.data.symbols;
-    const calculateYExtentsPrice = (d) => {
-      let extents = [];
-      symbols.forEach(s => {
-        let name = s.name;
-         extents.push(d[name].close);
-         d[name].high && extents.push(d[name].high);
-         d[name].low && extents.push(d[name].low);
-       });
-      return extents;
-    };
-
-    const calculateYExtentsMarketCap = (d) => {
-      let extents = [];
-      symbols.forEach(s => {
-        let name = s.name;
-        d[name].cap && extents.push(d[name].cap);
-      });
-      return extents;
-    };
-
-    const margin = this.props.margin;
+  constructor(props) {
+    super(props);
+    this.onDragStart = this.onDragStart.bind(this);
+    this.onDragEnd = this.onDragEnd.bind(this);
     
-    return (
-      <ChartCanvas ratio={1} width={this.props.width} height={this.props.height}
-                   margin={margin}
-                   type={'svg'}
-                   pointsPerPxThreshold={1}
-                   seriesName="GoldAndBTC"
-                   data={data}
-                   xAccessor={xAccessor}
-                   displayXAccessor={displayXAccessor}
-                   xScale={xScale}
-                   xExtents={xExtents}>
-
-          <Chart id={1}
-                 yExtents={calculateYExtentsPrice}>
-              <XAxis
-                axisAt="bottom"
-                orient="bottom"
-                innerTickSize={-1 * (this.props.height - margin.top - margin.bottom)}
-                tickStrokeDasharray='Solid'
-                tickStrokeOpacity={0.2}
-                tickStrokeWidth={0.5}
-              />
-              <YAxis
-                axisAt="right"
-                orient="right"
-                ticks={5}
-                innerTickSize={-1 * (this.props.width - margin.left - margin.right)}
-                tickStrokeDasharray='Solid'
-                tickStrokeOpacity={0.2}
-                tickStrokeWidth={0.5}
-              />
-              <MouseCoordinateX
-	        at="bottom"
-	        orient="bottom"
-	        displayFormat={timeFormat("%Y-%m-%d")} />
-	      <MouseCoordinateY
-	        at="right"
-	        orient="right"
-	        displayFormat={format(".2f")} />
-              {
-                symbols.map(s => {
-                  return <LineSeries
-                           key={s.name}
-                           yAccessor={d => d[s.name].close}
-                           stroke={s.color}
-                           strokeWidth={2}
-                  />
-                })
-              }
-              <HoverTooltip 
-                tooltipContent={tooltipContent(symbols)}
-                fontSize={15}
-              />
-          </Chart>
-          {showMarketCap && 
-          <Chart id={2}
-                 yExtents={calculateYExtentsMarketCap}>
-              <YAxis
-                axisAt="left"
-                orient="left"
-                ticks={5}
-                tickFormat={format(".0s")}
-                innerTickSize={-1 * this.props.width}
-                tickStrokeDasharray='Solid'
-                tickStrokeOpacity={0.2}
-                tickStrokeWidth={0.5}
-              />
-          </Chart>}
-          <CrossHairCursor />
-      </ChartCanvas>
-      
-
-      
-    );
-
+    this.state = {
+      isDragging: false,
+    };
   }
   
+  onDragStart(e) {
+    debugger;
+    this.setState({isDragging: true});
+  }
+
+  onDrag(shiftCb, spacing, e) {
+    if (this.state.isDragging) {
+      // we want to go the opposite way of the actual movement
+      const sign = -Math.sign(e.movementX);
+      const extentsShifted = Math.ceil(Math.abs(e.movementX) / Math.floor(spacing));
+      extentsShifted > 0 && shiftCb(sign * extentsShifted);
+    }
+  }
+
+  onDragEnd(e) {
+    if (this.state.isDragging) {
+      this.setState({isDragging: false});
+    }
+  }
+
+
+  render() {
+    const width = this.props.width;
+    const height = this.props.height;
+    const margin = this.props.margin;
+    const dayCount = this.props.dayCount;
+    const data = this.props.data;
+    this.shift = this.props.shift;
+    
+    const xMax = width - margin.left - margin.right;
+    const yMax = height - margin.top - margin.bottom;
+
+    const x = d => new Date(d.date);
+    const symbols = this.props.symbols;
+    
+    const maxYScale = (d) => Math.max(...symbols.map(s => d[s.name].close));
+    
+    const xScale = scaleTime({
+      range: [0, xMax],
+      domain: extent(data, x)
+    });
+    const yScale = scaleLinear({
+      range: [yMax, 0],
+      domain: [0, max(data, maxYScale)],
+    });
+
+    // pixel space between datums, required to calculate how chart shifts
+    // when dragged
+    const xSpacing = xScale(data[1].date) - xScale(data[0].date);
+    const shiftCb = this.props.shiftCb;
+    const doDrag = this.onDrag.bind(this, shiftCb, xSpacing);
+    
+    const linePath = (symbol, data) => {
+      const y = (d) => d[symbol.name].close;
+      return (
+        <LinePath
+          key={symbol.name}
+          data={data}
+          xScale={xScale}
+          yScale={yScale}
+          x={x}
+          y={y}
+          stroke={symbol.color}
+          strokeWidth={2}
+        />
+      );
+    };
+
+    // const linePaths = this.props.data.symbols.map(s => linePath(s.name));
+    return (
+      <svg width={width} height={height}>
+          <Group top={margin.top} left={margin.left} style={{cursor: 'move'}}
+                 draggable="true"
+                 onMouseMove={doDrag}
+                 onMouseUp={this.onDragEnd}
+                 onMouseDown={(e) => this.onDragStart(e)}>
+              <rect
+                width={width}
+                height={height}
+                style={{pointerEvents: 'all', visibility: 'hidden'}}
+                onMouseMove={doDrag}
+                onMouseUp={this.onDragEnd}
+                onMouseDown={this.onDragStart}>
+              </rect>
+              {symbols.map(s => linePath(s, data))}
+              <AxisBottom
+                scale={xScale}
+                top={yMax}
+                stroke='#1b1a1e'
+                tickTextFill="#1b1a1e"
+              />
+              <AxisRight
+                scale={yScale}
+                top={0}
+                left={width - margin.left - margin.right}
+                label='Close Price ($)'
+                labelOffset={50}
+                labelProps={{fontSize: 15}}
+                stroke="#1b1a1e"
+                tickTextFill="#1b1a1e"
+              />
+          </Group>
+      </svg>
+    );
+  }
 }
 
-export default HotAirChart;
+
+    
+
+    /* import { timeFormat } from "d3-time-format";
+     * import { format } from "d3-format";
+     * import {Chart, ChartCanvas} from 'react-stockcharts/es';
+     * import { XAxis, YAxis } from "react-stockcharts/es/lib/axes"
+     * import { LineSeries } from 'react-stockcharts/es/lib/series';
+     * import { discontinuousTimeScaleProvider } from "react-stockcharts/es/lib/scale";
+     * import { last } from "react-stockcharts/es/lib/utils";
+     * import {
+     *   CrossHairCursor,
+     *   MouseCoordinateX,
+     *   MouseCoordinateY,
+     * } from "react-stockcharts/es/lib/coordinates";
+     * import { HoverTooltip } from "react-stockcharts/es/lib/tooltip";
+     * 
+     * 
+     * const formatNum = (symbol, num) => {
+     *   let numFormatted = '$ ' + num.toFixed().toString().replace(/(\d)(?=(\d{3})+(?:\.\d+)?$)/g, "$1,");
+     *   if (symbol.measure) {
+     *     numFormatted += symbol.measure;
+     *   }
+     *   // this magic incantation adds commas in the thousands place
+     *   return numFormatted
+     * };
+     * 
+     * // remove the hours:minutes:seconds
+     * const formatDate = (date) => {
+     *   return date.toUTCString().split(/\ \d\d:\d\d:\d\d/)[0];
+     * };
+     * 
+     * const tooltipContent = (symbols) => {
+     *   return (d) => {
+     *     const {currentItem, xAccessor} = d;
+     *     const yValues = symbols.map(s => {
+     *       let items = [
+     *         {
+     *           label: s.name + ' ',
+     *           value: formatNum(s, currentItem[s.name].close),
+     *           stroke: '#000000'
+     *         },
+     *       ];
+     *       if (currentItem[s.name].cap) {
+     *         items.push(
+     *           {label: 'Market Cap',
+     *            value: '$ ' + format('.0s')(currentItem[s.name].cap),
+     *            stroke: '#000000'}
+     *         );
+     *       }
+     *       return items;
+     *     }).reduce((accum, arr) => accum.concat(arr), []);
+     *     return {
+     *       x: formatDate(xAccessor(currentItem)),
+     *       y: yValues,
+     *     };
+     *   };
+     * };
+     * 
+     * class HotAirChart extends React.Component {
+     *   render() {
+     *     const showMarketCap = this.props.showMarketCap;
+     *     const xScaleProvider = discontinuousTimeScaleProvider
+     *       .inputDateAccessor(d => d.date);
+     *     const {
+     *       data,
+     *       xScale,
+     *       xAccessor,
+     *       displayXAccessor,
+     *     } = xScaleProvider(this.props.data);
+     * 
+     *     const xExtents = [
+     *       xAccessor(data[data.length - this.props.dayCount]),
+     *       xAccessor(last(data)),
+     *     ];
+     * 
+     *     const symbols = this.props.data.symbols;
+     *     const calculateYExtentsPrice = (d) => {
+     *       let extents = [];
+     *       symbols.forEach(s => {
+     *         let name = s.name;
+     *          extents.push(d[name].close);
+     *          d[name].high && extents.push(d[name].high);
+     *          d[name].low && extents.push(d[name].low);
+     *        });
+     *       return extents;
+     *     };
+     * 
+     *     const calculateYExtentsMarketCap = (d) => {
+     *       let extents = [];
+     *       symbols.forEach(s => {
+     *         let name = s.name;
+     *         d[name].cap && extents.push(d[name].cap);
+     *       });
+     *       return extents;
+     *     };
+     * 
+     *     const margin = this.props.margin;
+     *     
+     *     return (
+     *       <ChartCanvas ratio={1} width={this.props.width} height={this.props.height}
+     *                    margin={margin}
+     *                    type={'svg'}
+     *                    pointsPerPxThreshold={1}
+     *                    seriesName="GoldAndBTC"
+     *                    data={data}
+     *                    xAccessor={xAccessor}
+     *                    displayXAccessor={displayXAccessor}
+     *                    xScale={xScale}
+     *                    xExtents={xExtents}>
+     * 
+     *           <Chart id={1}
+     *                  yExtents={calculateYExtentsPrice}>
+     *               <XAxis
+     *                 axisAt="bottom"
+     *                 orient="bottom"
+     *                 innerTickSize={-1 * (this.props.height - margin.top - margin.bottom)}
+     *                 tickStrokeDasharray='Solid'
+     *                 tickStrokeOpacity={0.2}
+     *                 tickStrokeWidth={0.5}
+     *               />
+     *               <YAxis
+     *                 axisAt="right"
+     *                 orient="right"
+     *                 ticks={5}
+     *                 innerTickSize={-1 * (this.props.width - margin.left - margin.right)}
+     *                 tickStrokeDasharray='Solid'
+     *                 tickStrokeOpacity={0.2}
+     *                 tickStrokeWidth={0.5}
+     *               />
+     *               <MouseCoordinateX
+     * 	        at="bottom"
+     * 	        orient="bottom"
+     * 	        displayFormat={timeFormat("%Y-%m-%d")} />
+     * 	      <MouseCoordinateY
+     * 	        at="right"
+     * 	        orient="right"
+     * 	        displayFormat={format(".2f")} />
+     *               {
+     *                 symbols.map(s => {
+     *                   return <LineSeries
+     *                            key={s.name}
+     *                            yAccessor={d => d[s.name].close}
+     *                            stroke={s.color}
+     *                            strokeWidth={2}
+     *                   />
+     *                 })
+     *               }
+     *               <HoverTooltip 
+     *                 tooltipContent={tooltipContent(symbols)}
+     *                 fontSize={15}
+     *               />
+     *           </Chart>
+     *           {showMarketCap && 
+     *           <Chart id={2}
+     *                  yExtents={calculateYExtentsMarketCap}>
+     *               <YAxis
+     *                 axisAt="left"
+     *                 orient="left"
+     *                 ticks={5}
+     *                 tickFormat={format(".0s")}
+     *                 innerTickSize={-1 * this.props.width}
+     *                 tickStrokeDasharray='Solid'
+     *                 tickStrokeOpacity={0.2}
+     *                 tickStrokeWidth={0.5}
+     *               />
+     *           </Chart>}
+     *           <CrossHairCursor />
+     *       </ChartCanvas>
+     *       
+     * 
+     *       
+     *     );
+     * 
+     *   }
+     *   
+     * } */
+
+    export default HotAirChart;
